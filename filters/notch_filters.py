951b5ed25177ce0e6c8a0e7af58ae8f25dd7d86f
unknown <deepraval2905@gmail.com>
2020-08-24 02:03:55 +0530

    Added all working filters and status label

--- filters/notch_filters.py
+++ filters/notch_filters.py
@@ -2,21 +2,27 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib
+from math import exp, pow
 
 class IdealNotchFilter:
     def __init__(self):
         pass
     
-    def apply_filter(self, fshift, points, freq, path):
-        #Apply filter
-        for i in range(fshift.shape[0]):
-            for j in range(fshift.shape[1]):
-                for k in range(len(points)):
-                    m = points[k][0]
-                    n = points[k][1]
-                    if(pow(n - i, 2) + pow(m - j, 2) <= freq):
-                        fshift[i][j] = 0
-
+    def apply_filter(self, fshift, points, d0, path):
+        m = fshift.shape[0]
+        n = fshift.shape[1]
+        for u in range(m):
+            for v in range(n):
+                for d in range(len(points)):
+                    u0 = points[d][0]
+                    v0 = points[d][1]
+                    u0, v0 = v0, u0
+                    d1 = pow(pow(u - u0, 2) + pow(v - v0, 2), 1)
+                    d2 = pow(pow(u + u0, 2) + pow(v + v0, 2), 1)
+                    if d1 <= d0 or d2 <= d0:
+                        fshift[u][v] *= 0.0
+                    #if pow(v - u0, 2) + pow(u - v0, 2) <= d0:
+                    #    fshift[u][v] = 0.0
         f_ishift = np.fft.ifftshift(fshift)
         img_back = np.fft.ifft2(f_ishift)
         img_back = np.abs(img_back)
@@ -27,16 +33,19 @@ class ButterworthNotchFilter:
     def __init__(self):
         pass
     
-    def apply_filter(self, fshift, points, freq, path):
-        #Apply filter
-        for i in range(fshift.shape[0]):
-            for j in range(fshift.shape[1]):
-                for k in range(len(points)):
-                    m = points[k][0]
-                    n = points[k][1]
-                    if(((m - j)*(m-j) )+((n - i)*(n-i)) < freq):
-                        fshift[i][j] = 0
-
+    def apply_filter(self, fshift, points, d0, path, order = 1):
+        m = fshift.shape[0]
+        n = fshift.shape[1]
+        for u in range(m):
+            for v in range(n):
+                for d in range(len(points)):
+                    u0 = points[d][0]
+                    v0 = points[d][1]
+                    u0, v0 = v0, u0
+                    d1 = pow(pow(u - u0, 2) + pow(v - v0, 2), 0.5)
+                    d2 = pow(pow(u + u0, 2) + pow(v + v0, 2), 0.5)
+                    fshift[u][v] *= (1.0 / (1 + pow((d0 * d0) / (d1 * d2), order))) 
+                    
         f_ishift = np.fft.ifftshift(fshift)
         img_back = np.fft.ifft2(f_ishift)
         img_back = np.abs(img_back)
@@ -47,15 +56,18 @@ class GaussianNotchFilter:
     def __init__(self):
         pass
     
-    def apply_filter(self, fshift, points, freq, path):
-        #Apply filter
-        for i in range(fshift.shape[0]):
-            for j in range(fshift.shape[1]):
-                for k in range(len(points)):
-                    m = points[k][0]
-                    n = points[k][1]
-                    if(((m - j)*(m-j) )+((n - i)*(n-i)) < freq):
-                        fshift[i][j] = 0
+    def apply_filter(self, fshift, points, d0, path):
+        m = fshift.shape[0]
+        n = fshift.shape[1]
+        for u in range(m):
+            for v in range(n):
+                for d in range(len(points)):
+                    u0 = points[d][0]
+                    v0 = points[d][1]
+                    u0, v0 = v0, u0
+                    d1 = pow(pow(u - u0, 2) + pow(v - v0, 2), 0.5)
+                    d2 = pow(pow(u + u0, 2) + pow(v + v0, 2), 0.5)
+                    fshift[u][v] *= (1 - exp(-0.5 * (d1 * d2 / pow(d0, 2))))
 
         f_ishift = np.fft.ifftshift(fshift)
         img_back = np.fft.ifft2(f_ishift)
--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class MainApp:
         self.left_frame.rowconfigure(2)
         self.left_frame.rowconfigure(3)
         self.left_frame.rowconfigure(4)
+        self.left_frame.rowconfigure(5)
         self.original_img = tk.Label(self.left_frame, image = "", text = "Load an image \n to preview it here!", padx = 150, pady = 150)
         self.btn_browse_img = tk.Button(self.left_frame, text = "Browse Image", bg = "lightblue", command = self.browse_img)
         self.btn_apply_filter = tk.Button(self.left_frame, text = "Apply Filter", bg = "lightblue", command = self.apply_filter)
@@ -35,6 +36,7 @@ class MainApp:
         self.select_filter = tk.OptionMenu(self.left_frame, self.select_filter_var, *{'Ideal', 'Butterworth', 'Gaussian'})
         self.number_of_points = tk.Entry(self.left_frame)
         self.frequency = tk.Entry(self.left_frame)
+        self.butterworth_order = tk.Entry(self.left_frame)
         self.original_img.grid(row = 0, column = 0, columnspan = 2)
         self.btn_browse_img.grid(row = 1, column = 0, sticky='nsew')
         self.btn_apply_filter.grid(row = 1, column = 1, sticky='nsew')
@@ -43,18 +45,23 @@ class MainApp:
         tk.Label(self.left_frame, text = "Number of Points: ").grid(row = 3, column = 0, sticky = 'nsew')
         self.number_of_points.grid(row = 3, column = 1, sticky = 'nsew')
         self.number_of_points.insert(tk.END, '6')
-        tk.Label(self.left_frame, text = "Cut-off frequency: ").grid(row = 4, column = 0, sticky = 'nsew')
+        tk.Label(self.left_frame, text = "Band Radius: ").grid(row = 4, column = 0, sticky = 'nsew')
         self.frequency.grid(row = 4, column = 1, sticky = 'nsew')
         self.frequency.insert(tk.END, '121.0')
+        tk.Label(self.left_frame, text = "Order of \n Butterworth Filter").grid(row = 5, column = 0, sticky = 'nsew')
+        self.butterworth_order.grid(row = 5, column = 1, sticky = 'nsew')
+        self.butterworth_order.insert(tk.END, 1)
         self.left_frame.pack(side = "left", fill = tk.Y)
         #setting up Right side of GUI
         self.right_frame = tk.LabelFrame(text = "Filtered Image")
         self.filter_img = tk.Label(self.right_frame, image = "", text = "Apply filter to an image\nto view it here !", padx = 150, pady = 150)
         self.btn_save_img = tk.Button(self.right_frame, text = "Save this Image", bg = "lightblue", command = self.save_img)
         self.btn_summary = tk.Button(self.right_frame, text = "Show Summary",  bg = "lightblue", command = self.show_summary)
+        self.info_lbl = tk.Label(self.right_frame, text = "READY")
         self.filter_img.pack()
         self.btn_save_img.pack(in_ = self.right_frame, fill = tk.X)
         self.btn_summary.pack(in_ = self.right_frame, fill = tk.X)
+        self.info_lbl.pack(in_ = self.right_frame, fill = tk.BOTH, side = tk.LEFT)
         self.right_frame.pack(side = "right", fill = tk.Y) 
         
     def browse_img(self):
@@ -69,32 +76,36 @@ class MainApp:
         except Exception as e:
             messagebox.showerror("An error occured !", e)
 
-    def get_fshift_and_save_magnitude_spectrum(self):
+    def get_fshift_and_save_dft(self):
         img = Image.open(pathlib.Path("tmp/original_img.png"))
         img = np.asarray(img)
         f = np.fft.fft2(img)
         fshift = np.fft.fftshift(f)
         dft = 20 * np.log(np.abs(fshift))
         matplotlib.image.imsave(pathlib.Path("tmp/dft.png"), dft, cmap = "gray")
-        return fshift
+        return fshift, dft
                 
     def apply_filter(self):
         try:
-            fshift = self.get_fshift_and_save_magnitude_spectrum()
+            self.info_lbl.configure(text = "BUSY")
+            self.info_lbl.text = "BUSY"
+            fshift, dft = self.get_fshift_and_save_dft()
             plt.clf()
             plt.imshow(Image.open(pathlib.Path("tmp/dft.png")), cmap = "gray")
             set_plot_title("Click on image to choose points. (Press any key to Start)")
             plt.waitforbuttonpress()
             set_plot_title(f'Select {self.number_of_points.get()} points with mouse click')
-            points = np.asarray(plt.ginput(int(self.number_of_points.get()), timeout = -1))
+            points = np.asarray(plt.ginput(int(self.number_of_points.get()), timeout = -1))     
             plt.close()
             #Applying filter
             if self.select_filter_var.get() == "Ideal":
                 IdealNotchFilter().apply_filter(fshift, points, float(self.frequency.get()), pathlib.Path("tmp/filtered_img.png"))
             elif self.select_filter_var.get() == "Butterworth":
-                ButterworthNotchFilter().apply_filter(fshift, points, float(self.frequency.get()), pathlib.Path("tmp/filtered_img.png"))
+                ButterworthNotchFilter().apply_filter(fshift, points, float(self.frequency.get()), pathlib.Path("tmp/filtered_img.png"), order = int(self.butterworth_order.get()))
             elif self.select_filter_var.get() == "Gaussian":
                 GaussianNotchFilter().apply_filter(fshift, points, float(self.frequency.get()), pathlib.Path("tmp/filtered_img.png"))
+            self.info_lbl.configure(text = "READY")
+            self.info_lbl = "READY"
             #Show filtered image
             filter_img = ImageTk.PhotoImage(ImageOps.grayscale((Image.open(pathlib.Path("tmp/filtered_img.png")))))
             self.filter_img.configure(text = "", image = filter_img)
@@ -109,7 +120,7 @@ class MainApp:
             Image.open(pathlib.Path("tmp/filtered_img.png")).save(directory)
         except Exception as e:
             messagebox.showerror("An error occured!", e)        
-    def save_magnitude_spectrum(self, path, save_path):
+    def save_dft(self, path, save_path):
         img = ImageOps.grayscale((Image.open(path)))
         img = np.asarray(img)
         f = np.fft.fft2(img)
@@ -122,8 +133,8 @@ class MainApp:
         axarr[0,0].imshow(Image.open(pathlib.Path("tmp/original_img.png")), cmap = "gray")
         axarr[0,1].imshow(Image.open(pathlib.Path("tmp/filtered_img.png")), cmap = "gray")
         axarr[1,0].imshow(Image.open(pathlib.Path("tmp/dft.png")), cmap = "gray")
-        self.save_magnitude_spectrum(pathlib.Path("tmp/filtered_img.png"), pathlib.Path("tmp/tdft"))
-        axarr[1,1].imshow(Image.open(pathlib.Path("tmp/tdft")), cmap = "gray")
+        self.save_dft(pathlib.Path("tmp/filtered_img.png"), pathlib.Path("tmp/tdft.png"))
+        axarr[1,1].imshow(Image.open(pathlib.Path("tmp/tdft.png")), cmap = "gray")
         plt.show()
         
     def run(self):
